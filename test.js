a510c2495ced14f493d5e3f2a43cecc54f902d6d
zoverlvx <zoverhulser@gmail.com>
2018-05-06 17:59:54 -0400

    Greets user personally.

--- test.js
+++ test.js
@@ -5,11 +5,14 @@
 // http://www.jslint.com/help.html
 const assert = require("assert");
 
-function makeGreeting() {
+function makeGreeting(name) {
+    if (name) {
+        return "Hello " + name + "!";
+    }
     "use strict";
     return "Hello world!";
 }
 
 assert.strictEqual(makeGreeting(), "Hello world!");
-assert.strictEqual(makeGreeting(), "Hello Mike!");
+assert.strictEqual(makeGreeting("Mike"), "Hello Mike!");
 console.log("All tests have passed");
