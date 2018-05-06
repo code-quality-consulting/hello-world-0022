d4d82acda2e9de88983a08ac033f5385ab59fffc
zoverlvx <zoverhulser@gmail.com>
2018-05-06 17:52:36 -0400

    JSLints from source.

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
+/*jslint
+    node
+*/
+
+// http://www.jslint.com/help.html
 const assert = require("assert");
 
-// https://nodejs.org/docs/latest-v8.x/api/assert.html
+function makeGreeting() {
+    "use strict";
+    return undefined;
+}
 
-function makeGreeting() {}
-
-// https://nodejs.org/docs/latest-v8.x/api/assert.html#assert_assert_strictequal_actual_expected_message
 assert.strictEqual(makeGreeting(), "Hello world!");
